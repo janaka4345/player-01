1af865c50480c3a173a8102ad5079b284b352829
J_ Keyy <ashnazg1212@gmail.com>
2023-11-28 02:02:47 +0000

    Update World.jsx and yarn.lock

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,15 +1,28 @@
-import { OrbitControls } from "@react-three/drei";
+import { Grid, OrbitControls } from "@react-three/drei";
 import Lights from "./Lights";
-import { Physics } from "@react-three/rapier";
+import { Physics, RigidBody } from "@react-three/rapier";
+import { SphereGeometry } from "three";
 
 export default function World() {
   return (
     <>
       <OrbitControls makeDefault />
       <axesHelper args={[2]} />
-      <color attach="background" args={["#000000"]} />
-      <Physics>
+      <color attach="background" args={["#ffffff"]} />
+      <Physics debug>
         <Lights />
+        <RigidBody type="fixed" colliders="hull">
+          <mesh rotation={[-Math.PI * 0.5, 0, 0]}>
+            <planeGeometry args={[10, 10]} />
+            <meshBasicMaterial color={"green"} />
+          </mesh>
+        </RigidBody>
+        <RigidBody colliders="ball" position={[0.5, 5, 0]}>
+          <mesh>
+            <sphereGeometry />
+            <meshBasicMaterial color={"red"} />
+          </mesh>
+        </RigidBody>
       </Physics>
     </>
   );
