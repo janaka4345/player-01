70f88e87e9c1403755573ae327cd5c290bea635c
J_ Keyy <ashnazg1212@gmail.com>
2023-11-28 02:39:15 +0000

    Update 2 files

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,9 +1,13 @@
-import { Grid, OrbitControls } from "@react-three/drei";
+import { Grid, OrbitControls, useTexture } from "@react-three/drei";
 import Lights from "./Lights";
 import { Physics, RigidBody } from "@react-three/rapier";
-import { SphereGeometry } from "three";
+import { RepeatWrapping, SphereGeometry } from "three";
 
 export default function World() {
+  const texture = useTexture("./green.jpg");
+  texture.wrapS = RepeatWrapping;
+  texture.wrapT = RepeatWrapping;
+  texture.repeat.set(10, 10);
   return (
     <>
       <OrbitControls makeDefault />
@@ -11,13 +15,23 @@ export default function World() {
       <color attach="background" args={["#ffffff"]} />
       <Physics debug>
         <Lights />
-        <RigidBody type="fixed" colliders="hull">
+        <RigidBody
+          type="fixed"
+          colliders="hull"
+          restitution={0.2}
+          friction={0.7}
+        >
           <mesh rotation={[-Math.PI * 0.5, 0, 0]}>
-            <planeGeometry args={[10, 10]} />
-            <meshBasicMaterial color={"green"} />
+            <planeGeometry args={[100, 100, 10, 10]} />
+            <meshBasicMaterial map={texture} />
           </mesh>
         </RigidBody>
-        <RigidBody colliders="ball" position={[0.5, 5, 0]}>
+        <RigidBody
+          colliders="ball"
+          position={[15, 5, 0]}
+          restitution={0.5}
+          friction={0.2}
+        >
           <mesh>
             <sphereGeometry />
             <meshBasicMaterial color={"red"} />
